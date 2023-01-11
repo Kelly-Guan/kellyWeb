ffb123d0cc39db22ccb7fe93dadb806455d9e19e
Kelly Guan <kellyguan@v1040-wn-rt-a-99-44.campus-dynamic.uwaterloo.ca>
2023-01-11 17:11:11 -0500

    wordle

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import linkedin from '../imgs/socialMedia/linkedin.png';
 import email from '../imgs/socialMedia/gmail.png';
 import github from '../imgs/socialMedia/github.png';
-import devpost from '../imgs/socialMedia/devpost.png';
+import wordle from '../imgs/socialMedia/wordle.png';
 
 import '../App.css';
 
@@ -13,7 +13,7 @@ export default function socialMedia() {
           <a href="https://www.linkedin.com/in/kelly-guan/" target="_blank" rel="noreferrer"><img src={linkedin} alt=""/></a>
           <a href="mailto: kelly.g.guan@gmail.com" target="_blank" rel="noreferrer"><img src={email} alt=""/></a>
           <a href="https://github.com/Kelly-Guan" target="_blank" rel="noreferrer"><img src={github} alt=""/></a>
-          <a href="https://www.kellogswords.rocks/" target="_blank" rel="noreferrer"><img src={devpost} alt=""/></a>
+          <a href="https://www.kellogswords.rocks/" target="_blank" rel="noreferrer"><img src={wordle} alt=""/></a>
         </div>
     )
 }
