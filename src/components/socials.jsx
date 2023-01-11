9697a921870ca03ed5b4c81370d60d54c276ebc4
Kelly Guan <kellyguan@v1040-wn-rt-a-99-44.campus-dynamic.uwaterloo.ca>
2023-01-11 17:08:49 -0500

    wordle?

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -13,7 +13,7 @@ export default function socialMedia() {
           <a href="https://www.linkedin.com/in/kelly-guan/" target="_blank" rel="noreferrer"><img src={linkedin} alt=""/></a>
           <a href="mailto: kelly.g.guan@gmail.com" target="_blank" rel="noreferrer"><img src={email} alt=""/></a>
           <a href="https://github.com/Kelly-Guan" target="_blank" rel="noreferrer"><img src={github} alt=""/></a>
-          <a href="https://devpost.com/KellyGuan" target="_blank" rel="noreferrer"><img src={devpost} alt=""/></a>
+          <a href="https://www.kellogswords.rocks/" target="_blank" rel="noreferrer"><img src={devpost} alt=""/></a>
         </div>
     )
 }
